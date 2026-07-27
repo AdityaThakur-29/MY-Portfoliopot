c17f16ef759fad56b02226d9915f0c711cb6a447
AdityaThakur-29 <adityathakur77749@gmail.com>
2026-07-27 23:56:38 +0530

    responsibal

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import dynamic from 'next/dynamic'
 import { ReactLenis } from 'lenis/react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
@@ -7,10 +8,15 @@ import Projects from '../components/Projects'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
 
+const TargetCursor = dynamic(() => import('../components/TargetCursor'), {
+  ssr: false
+})
+
 export default function Home() {
   return (
     <ReactLenis root>
       <div>
+        <TargetCursor targetSelector=".cursor-target, button, a, .project-stack-card, .skill-category-card, .about-feature-card" />
         <Navbar />
         <Hero />
         <About />
